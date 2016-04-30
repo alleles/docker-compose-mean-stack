65ed7c5508aaac4d24f56bf38ef5e40d271e500f
stoked <brianstoker@gmail.com>
2016-04-30 03:28:43 -0500

    AB-13 - simple stack - needs consul and mongodb to complete poc

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-triton test 
+joyent triton - poc
+
+prereq
+ - joyent account
+ - triton cli setup
+ - docker
+
+1. clone repo
+2. docker-compose build
+3. docker-compose up
+
+the thing is running (unless you suck at life)
+
+verify
+ - log into joyent account
+ - go to instances
+ - http://{dockerstack_nginx_1's ip}
--- node/index.js
+++ node/index.js
@@ -1,10 +1,29 @@
-var express = require('express'),
-    http = require('http'),
-    redis = require('redis');
+var express = require('express');
+var http = require('http');
+var redis = require('redis');
+var consul = require('consul')({ host: 'dockerstack_consul_1' });
+var port = (process.env.PORT || 8080);
+
+/*
+var consulOptions = { 
+	name: 'autoBuyer-web',
+    id: 'ab-web',
+	tags: ['service'],
+	port: port
+};
+
+setTimeout(function(){
+    consul.agent.service.register(consulOptions, function(err) {
+        if (err) {
+            console.log('CONSUL REGISTRATION ERROR', err);
+        }
+    });
+
+    console.log('PROCESS.ENV', process.env)
+}, 10000);
+*/
 
 var app = express();
-
-console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 
 // APPROACH 1: Using environment variables created by Docker
 // var client = redis.createClient(
@@ -20,9 +39,10 @@ app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
     if(err) return next(err);
     res.send('This page has been viewed ' + counter + ' times!');
+    console.log(req.baseUrl, port);
   });
 });
 
 http.createServer(app).listen(process.env.PORT || 8080, function() {
-  console.log('Listening on port ' + (process.env.PORT || 8080));
+  console.log('Listening on port ' + port);
 });
