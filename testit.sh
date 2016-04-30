5964c403a9bb83b1b64f48338649e4778c978f77
stoked <brianstoker@gmail.com>
2016-04-30 03:28:43 -0500

    AB-13 - simple stack - needs consul and mongodb to complete poc

--- testit.sh
+++ testit.sh
@@ -1,2 +1,3 @@
 for ((i=1;i<=100;i++)); 
- do   curl -v  -H 'Connection: close' "192.168.99.100"; done
+# do   curl -v  -H 'Connection: close' "64.30.129.66"; done
+do   curl -v  -H 'Connection: close' "192.168.99.100"; done
